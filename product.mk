10abaf2b14dad59290fd2a8864bf820505ada2b0
Haibo Gu <ghbhaha@users.noreply.github.com>
2017-03-26 13:16:11 +0800

    Update product.mk

--- product.mk
+++ product.mk
@@ -2,9 +2,3 @@
 PRODUCT_PACKAGES += \
     ThemeInterfacer \
     projekt.substratum.theme.xml
-
-# Add overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.opa.eligible_device=true
